8ee25375a04c8ee7cee21dd6a977f31fcaeaa7bf
joaopedrofonseca <joaopedrofonsec4@gmail.com>
2023-03-09 20:58:27 -0300

    feat: get top trendings

--- src/routers/trendingRouter.js
+++ src/routers/trendingRouter.js
@@ -1,6 +1,9 @@
 import { Router } from 'express'
-import { getTrending } from '../controllers/trendingController';
+import { getTrending, postTrending } from '../controllers/trendingController.js';
 
 const trendingRouter = Router();
 
+trendingRouter.post('/trending', postTrending)
 trendingRouter.get('/trending', getTrending)
+
+export default trendingRouter
