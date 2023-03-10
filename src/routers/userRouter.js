cf955ef8127a3f2aa989662fbc6a2ff38648c634
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-03-10 17:38:37 -0300

    feat: add validate token userRouter

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import {getUserById, searche} from "../controllers/userController.js"
 import authUser from '../middlewares/authUserMiddleware.js';
+import { tokenValidate } from '../middlewares/tokenValidate.js';
 
 const userRouter = Router();
 
-userRouter.get('/user/:id',authUser, getUserById);
-userRouter.get("/users", searche)
+userRouter.get('/user/:id',tokenValidate,authUser, getUserById);
+userRouter.get("/users",tokenValidate, searche)
 
 export default userRouter
