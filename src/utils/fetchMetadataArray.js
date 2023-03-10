3bde91197ca7210a68496b9224a4f5093b2a57aa
guilhiz <guilhermevilelasobral@gmail.com>
2023-03-10 12:10:08 -0300

    refact: fetch metadata

--- src/utils/fetchMetadataArray.js
+++ src/utils/fetchMetadataArray.js
@@ -1,13 +1,18 @@
 import urlMetadata from 'url-metadata';
 
 export const fetchMetadataArray = async (posts) => {
+  const fetchMetadata = async ({ id, name, photo, content, url }) => {
+    try {
+      const { title: urlTitle, description: urlDescription, image: urlImage } = await urlMetadata(url);
+      return { id, name, photo, content, url, urlTitle, urlDescription, urlImage };
+    } catch (error) {
+      console.error(`Error fetching metadata for URL '${url}': ${error}`);
+      return { id, name, photo, content, url };
+    }
+  };
+
   try {
-    const metadataArray = await Promise.all(
-      posts.map(async ({ id, name, photo, content, url }) => {
-        const { title: urlTitle, description: urlDescription, image: urlImage } = await urlMetadata(url);
-        return {id, name, photo, content, url, urlTitle, urlDescription, urlImage };
-      })
-    );
+    const metadataArray = await Promise.all(posts.map(fetchMetadata));
     return metadataArray;
   } catch (error) {
     throw new Error(`Error fetching metadata: ${error}`);
