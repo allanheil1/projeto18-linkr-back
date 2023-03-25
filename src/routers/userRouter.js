56a86e2915720b60677f1506d52ab493f324102c
Antonio-Jefferson <antjeffersonbatista@gmail.com>
2023-03-25 19:59:44 -0300

    userRout wheel change

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,6 +6,6 @@ import { tokenValidate } from '../middlewares/tokenValidate.js';
 const userRouter = Router();
 
 userRouter.get('/user/:id',tokenValidate, authUser, getUserById);
-userRouter.get("/users",tokenValidate, searche)
+userRouter.get("/users", searche)
 
 export default userRouter
